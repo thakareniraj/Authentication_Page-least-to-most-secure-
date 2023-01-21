b6520c4d4e2e053ee24d5eb2eecdb6f562bad59f
thakareniraj <96050843+thakareniraj@users.noreply.github.com>
2023-01-21 19:17:30 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,96 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const md5 =require("md5");
+const mongoose = require('mongoose');
+const bcrypt=require("bcrypt");
+const saltRounds =10 ;
+// var encrypt  =require("mongoose-encryption")
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+
+console.log(process.env.API_KEY)
+// const secret ="this is secret"
+// console.log(secret)
+console.log(process.env.SECRET)
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser : true})
+ 
+const userSchema =new mongoose.Schema({
+  email:String,
+  password:String
+})
+
+const User =new mongoose.model("User",userSchema)
+// userSchema.plugin(encrypt, { secret: process.env.SECRET ,encryptedFields: ["password"] });
+//TODO
+
+app.get("/",function(req,res){
+  res.render("home");
+});
+
+app.get("/login",function(req,res){
+  res.render("login")
+});
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+app.post("/register",function(req,res){
+  bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+    const newUser =new User({
+      email: req.body.username,
+      password:hash
+    })
+      newUser.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }else{
+      res.send("unable to add to the server");
+    }
+  })
+  })
+  
+
+
+  // res.redirect("/")
+  // res.render("register");
+});
+
+app.post("/login",function(req,res){
+  const username =req.body.username ;
+  const password =req.body.password;
+
+ 
+    User.findOne({email :username },function(err, foundUser){
+      if(err){
+        result.send("errr occ")
+        // res.send("error in loging")
+      }else{
+        bcrypt.compare(password,foundUser.password,function(err,result){
+          if(result==true){
+            res.render("secrets")
+          }
+        })
+       
+      }
+  })
+  console.log(username)
+  // res.redirect("/")
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+})
