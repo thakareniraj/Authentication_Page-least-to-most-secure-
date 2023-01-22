0884ca4a8d56f86cf53ec02c48b3ea656d6137eb
thakareniraj <96050843+thakareniraj@users.noreply.github.com>
2023-01-22 22:18:26 +0530

    Added my project

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const md5 =require("md5");
 const mongoose = require('mongoose');
-const bcrypt=require("bcrypt");
-const saltRounds =10 ;
+// const bcrypt=require("bcrypt");
+// const saltRounds =10 ;
+const session =require("express-session")
+const passport=require("passport")
+const passportLocalMongoose =require("passport-local-mongoose")
 // var encrypt  =require("mongoose-encryption")
 
 
@@ -14,16 +17,28 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static("public"));
+app.use(express.static("public")); 
 
+app.use(session({
+  secret:"ourbloodysecret",
+  resave: false,
+  saveUninitialized : true,
+  // cookie: { secure: true }
+}))
+
+app.use(passport.initialize())
+app.use(passport.session());
 
 console.log(process.env.API_KEY)
 // const secret ="this is secret"
 // console.log(secret)
 console.log(process.env.SECRET)
+mongoose.set('strictQuery',true);
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser : true})
  
 const userSchema =new mongoose.Schema({
@@ -31,8 +46,16 @@ const userSchema =new mongoose.Schema({
   password:String
 })
 
+userSchema.plugin(passportLocalMongoose)
+
 const User =new mongoose.model("User",userSchema)
 // userSchema.plugin(encrypt, { secret: process.env.SECRET ,encryptedFields: ["password"] });
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 //TODO
 
 app.get("/",function(req,res){
@@ -47,46 +70,79 @@ app.get("/register",function(req,res){
   res.render("register");
 });
 
-app.post("/register",function(req,res){
-  bcrypt.hash(req.body.password,saltRounds,function(err,hash){
-    const newUser =new User({
-      email: req.body.username,
-      password:hash
-    })
-      newUser.save(function(err){
-    if(!err){
-      res.redirect("/");
+app.get("/secrets",function(req,res){
+      if(req.isAuthenticated()){
+        res.render("secrets")
+      }else{
+        res.redirect("/login");
+      }
+})
+
+app.get("/logout",function(req,res){
+  req.logout(function(err){
+    if(err){
+      console.log(err)
     }else{
-      res.send("unable to add to the server");
+        
+      res.redirect("/")
     }
   })
+})
+
+app.post("/register",function(req,res){
+
+
+  User.register({username:req.body.username},req.body.password,function(err,user){
+    if(err){
+      console.log(err)
+      res.redirect("/")
+    }else{
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/secrets")
+      })
+
+    }
+  })
+
+  // bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+  //   const newUser =new User({
+  //     email: req.body.username,
+  //     password:hash
+  //   })
+  //     newUser.save(function(err){
+  //   if(!err){
+  //     res.redirect("/");
+  //   }else{
+  //     res.send("unable to add to the server");
+  //   }
+  // })
   })
   
 
 
   // res.redirect("/")
   // res.render("register");
-});
+// });
 
 app.post("/login",function(req,res){
-  const username =req.body.username ;
-  const password =req.body.password;
+     const user = new User({
+      username:req.body.username,
+      password:req.body.password
+     });
 
- 
-    User.findOne({email :username },function(err, foundUser){
+     req.login(user,function(err){
       if(err){
-        result.send("errr occ")
-        // res.send("error in loging")
+        console.log("while logging we got error mate")
       }else{
-        bcrypt.compare(password,foundUser.password,function(err,result){
-          if(result==true){
-            res.render("secrets")
-          }
+        passport.authenticate("local")(req,res ,function(){
+          res.redirect("/secrets")
         })
-       
+        res.redirect("/secrets");
       }
-  })
-  console.log(username)
+     })
+
+
+
   // res.redirect("/")
 });
 
